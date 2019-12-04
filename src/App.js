efff4fd0106f285c990c35d44ad1f0db2524b502
Dharya02 <dharyasharma70@gmail.com>
2019-12-04 22:04:22 +0530

    Styling: Styled 'App' (Home Page)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 
 function App() {
@@ -7,11 +8,19 @@ function App() {
        {/* <div className="header">// #1
           Phone Directory
         </div> */}
-        <header/>
+        {/* <header/> #5
         <button>Add</button>
         <div>
           <span>Name</span><br />
-          <span>Phone</span>
+          <span>Phone</span> */}
+           <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
         </div>
     </div>
   );
